032878540e4e20be85fbc4ed548228ed47729e60
alezanchet <alexandrezanchet@gmail.com>
2020-05-21 21:31:54 -0300

    POST reposiories, validateRepositoryId

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,12 +10,30 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response,next){
+  const {id} = request.params
+
+  if(!isUuid(id)){
+    return response.status(400).json({error:"Invalid repository ID."})
+  }
+
+  return next()
+}
+
+app.use('/projects/:id', validateRepositoryId)
+
 app.get("/repositories", (request, response) => {
   // TODO
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs } = request.body
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
