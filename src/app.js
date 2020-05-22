f6904ef15ec27d3169d1c0a969566f59f354d091
alezanchet <alexandrezanchet@gmail.com>
2020-05-21 21:43:24 -0300

    PUT repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const { uuid } = require("uuidv4");
+const { uuid,isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -20,7 +20,7 @@ function validateRepositoryId(request, response,next){
   return next()
 }
 
-app.use('/projects/:id', validateRepositoryId)
+app.use('/repositories/:id', validateRepositoryId)
 
 app.get("/repositories", (request, response) => {
   return response.json(repositories)
@@ -37,7 +37,20 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const {title,url,techs} = request.body
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+  if (repositoryIndex < 0){
+    return response.status(400).json({error:"Repository not found."})
+  }
+
+  const repository = { title, url, techs }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
